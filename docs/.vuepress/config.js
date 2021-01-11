5809ddc5947ad386433467afe66f0ae74b47d44b
yanxu gong <745710524@qq.com>
2021-01-11 21:17:00 +0800

    add config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-11 21:03:00
+ * @LastEditTime: 2021-01-11 21:16:17
+ * @LastEditors: Please set LastEditors
+ * @Description: 配置文件的入口
+ * @FilePath: \blog\docs\.vuepress\config.js
+ */
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  base: '/blog/'
+}
