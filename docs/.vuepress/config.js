a857cec4abd6e05a51a6e2c48b4aa9be00e32e82
yanxu gong <745710524@qq.com>
2021-01-11 21:32:41 +0800

    update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,16 @@
 /*
  * @Author: your name
  * @Date: 2021-01-11 21:03:00
- * @LastEditTime: 2021-01-11 21:16:17
+ * @LastEditTime: 2021-01-11 21:32:28
  * @LastEditors: Please set LastEditors
  * @Description: 配置文件的入口
  * @FilePath: \blog\docs\.vuepress\config.js
  */
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around',
-  base: '/blog/'
+  // 部署站点的基础路径
+  base: '/blog/',
+  // 标题
+  title: "Yanxu Gong's Blog",
+  // 描述
+  description: 'Personal blog based on Github Action + Vuepress',
 }
