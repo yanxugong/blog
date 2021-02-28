3757c6729c0fd0eaec07f34b4876dcd8442f38ac
yanxu gong <745710524@qq.com>
2021-02-28 17:45:57 +0800

    论坛文章迁移到个人博客

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,28 @@
 /*
  * @Author: your name
  * @Date: 2021-01-11 21:03:00
- * @LastEditTime: 2021-01-11 21:32:28
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-02-28 17:00:22
+ * @LastEditors: yanxu gong
  * @Description: 配置文件的入口
  * @FilePath: \blog\docs\.vuepress\config.js
  */
 module.exports = {
   // 部署站点的基础路径
-  base: '/blog/',
+  base: "/blog/",
   // 标题
   title: "Yanxu Gong's Blog",
   // 描述
-  description: 'Personal blog based on Github Action + Vuepress',
-}
+  description: "Personal blog based on Github Action + Vuepress",
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+  theme: "@vuepress/theme-vue",
+  themeConfig: {
+    nav: [
+      { text: "学习", link: "/study/" },
+      { text: "摄影", link: "/photography/" },
+      { text: "情感", link: "/emotion/" },
+      { text: "关于", link: "/about" },
+    ],
+    sidebar: "auto",
+    repo: "yanxugong/blog",
+  },
+};
--- docs/study/README.md
+++ docs/study/README.md
@@ -1,1 +1,36 @@
-# 学习
+前端学习记录 📝。
+
+## 目录
+
+### 1. JavaScript 基础
+
+- [你不知道的 JavaScript（中卷）](/study/js-basis/you-do-not-konw-js)
+- [你不知道的 JavaScript（下卷）](/study/js-basis/you-do-not-konw-js-down)
+
+### 2. CSS 基础
+
+### 3. 框架
+
+- [深入 react 技术栈（一）](/study/frame/deep-react-one)
+- [深入 react 技术栈（二）](/study/frame/deep-react-two)
+
+### 4. 网络
+
+- [说一说从 URL 输入到页面呈现到底发生了什么？](/study/network/from-url-to-page-render)
+
+### 5. 数据结构与算法
+
+### 6. 安全
+
+### 7. 性能优化
+
+### 8. 工程化
+
+- [「webpack 核心特性」loader](/study/engineering/webpack-loader)
+- [「webpack 核心特性」插件(plugin)](/study/engineering/webpack-plugin)
+- [「webpack 核心特性」工作原理](/study/engineering/webpack-main)
+- [「webpack 核心特性」模块热替换(HMR)](/study/engineering/webpack-hmr)
+
+### 9. 服务端
+
+- [Go 快速入门指南](/study/server/go-getting-started)
